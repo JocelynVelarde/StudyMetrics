04937aefa074047d2a18835047813f860fd00a83
JocelynVelarde <a01285780@itesm.mx>
2024-08-17 10:36:44 -0600

    updated page config files

--- home.py
+++ home.py
@@ -1,38 +1,36 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
 st.set_page_config(
         page_title="StudyMonitor",
         page_icon="📚",
 )
 
-st.title(':green[Welcome to StudyMonitor 📚]')
+st.title(':orange[Welcome to StudyMonitor 📚]')
 
 st.write('Some random content in here')
 
 st.subheader('We have three main infrastructure features:')
 
 col1, col2 = st.columns(2)
-col1.subheader(":green[In Person Classroom Metrics]")
+col1.subheader(":orange[In Person Classroom Metrics]")
 col1.divider()
 col1.write("- ADD CONTENT HERE")
 
-col2.subheader(":green[Generate Content]")
+col2.subheader(":orange[Generate Content]")
 col2.divider()
 col2.write("- ADD CONTENT HERE")
 
-col3 = st.columns(2)
-col3.subheader(":green[Virtual Classroom Metrics]")
+col3, col4 = st.columns(2)
+col3.subheader(":orange[Virtual Classroom Metrics]")
 col3.divider()
 col3.write("- ADD CONTENT HERE")
 
 st.divider()
-st.subheader(":green[We combine the use of several tools to make your teaching experience easier]")
+st.subheader(":orange[We combine the use of several tools to make your teaching experience easier]")
 
 
 st.divider()
-st.subheader(":green[Get Started]")
+st.subheader(":orange[Get Started]")
 st.page_link("pages/instructions.py", label="See Instructions 🚀")
 
 st.divider()
